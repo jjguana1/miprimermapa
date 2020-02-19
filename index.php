b8e9b262b43c2a1b5f1282f83ca7457c87c295bd
jjguana1 <61239467+jjguana1@users.noreply.github.com>
2020-02-19 09:35:36 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+	error_reporting(0);
+	session_start();
+	session_destroy();
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<style>
+			html, body { height:100%; }
+		</style>
+		<script type="text/javascript" src="js/loader.js"></script>
+	</head>
+</html>
